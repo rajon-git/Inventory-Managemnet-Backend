1fcc4cd222c150be960d575ec4bc519caafe0335
MD. RAJON AHAMAD <117678976+rajon-git@users.noreply.github.com>
2023-07-13 01:00:04 +0600

    login route Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {Registration}= require ("../controllers/Users/UsersController")
+const {Registration,Login}= require ("../controllers/Users/UsersController")
 // users Router
 router.post("/registration",Registration);
+router.post("/login",Login);
 
 module.exports = router;
