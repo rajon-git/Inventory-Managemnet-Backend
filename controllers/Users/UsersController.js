858947d73883207ba293d01d605d1063fdecb27e
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-13 16:44:07 +0600

    controllers update

--- controllers/Users/UsersController.js
+++ controllers/Users/UsersController.js
@@ -27,37 +27,37 @@ const Registration = async (req, res) => {
 const Login = async (req, res) => {
   let result = await UserLoginService(req, UserModel);
   // user Response
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 const Read = async (req, res) => {
   let result = await UserDetailsService(req, UserModel);
   // user Response
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 const Update = async (req, res) => {
   let result = await UserUpdateService(req, UserModel);
   // user Response
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 const VerifyEmail = async (req, res) => {
   let result = await UserVerifyEmailService(req, UserModel);
   // user Response
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 const VerifyOtp = async (req, res) => {
   let result = await UserOtpVerifyService(req, UserModel);
   // user Response
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 const ResetPassword = async (req, res) => {
   let result = await UserResetPassService(req, UserModel);
   // user Response
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 module.exports ={Registration,Login,Read, Update, VerifyEmail, VerifyOtp, ResetPassword}
