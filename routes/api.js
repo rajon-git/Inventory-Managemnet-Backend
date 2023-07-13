5782180d81ad10ff57f37e7f1913d59dd89d97af
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-13 16:55:31 +0600

    users route added

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const {Registration,Login}= require ("../controllers/Users/UsersController")
+const AuthVerifyMiddleware =require("../middlewares/AuthVerifyMiddleware");
+const {Registration,Login,Read,Update,VerifyEmail,VerifyOtp,ResetPassword}= require ("../controllers/Users/UsersController")
 // users Router
 router.post("/registration",Registration);
 router.post("/login",Login);
+router.post("/profileupdate",AuthVerifyMiddleware,Update);
+router.get("/profiledetails",AuthVerifyMiddleware,Read);
+router.get("/recoverEmail/:email",VerifyEmail)
+router.get("/verifyOtp/:email/:otp",VerifyOtp)
+router.post("/resetPassword",ResetPassword)
+
 
 
 
