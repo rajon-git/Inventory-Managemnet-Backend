f78a798fc3f5ad667abd16a505224fdde9aff5b5
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-16 16:14:36 +0600

    category routes added

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const AuthVerifyMiddleware =require("../middlewares/AuthVerifyMiddleware");
 const {Registration,Login,Read,UpdateProfile,VerifyEmail,VerifyOtp,ResetPassword}= require ("../controllers/Users/UsersController")
 // Brand Controller modules
 const BrandsControllers = require('../controllers/brands/BrandsController');
+const CategoriesControllers=require("../controllers/category/CategoryController")
 // users Router
 router.post("/registration",Registration);
 router.post("/login",Login);
@@ -21,5 +22,13 @@ router.post("/createBrand",AuthVerifyMiddleware,BrandsControllers.CreateBrand);
 router.post("/updateBrand/:id",AuthVerifyMiddleware,BrandsControllers.UpdateBrand);
 router.get("/brandList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,BrandsControllers.BrandList);
 router.get("/brandDropDown",AuthVerifyMiddleware,BrandsControllers.BrandDropDown);
+
+// Categories Routing
+router.post("/createCategories",AuthVerifyMiddleware,CategoriesControllers.CreateCategories);
+router.post("/updateCategories/:id",AuthVerifyMiddleware,CategoriesControllers.UpdateCategories);
+router.get("/categoriesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,CategoriesControllers.CategoriesList);
+router.get("/categoriesDropDown",AuthVerifyMiddleware,CategoriesControllers.CategoriesDropDown);
+router.get("/deleteCategories/:id",AuthVerifyMiddleware,CategoriesControllers.DeleteCategories);
+router.get("/categoriesDetails/:id",AuthVerifyMiddleware,CategoriesControllers.CategoriesDetails);
 
 module.exports = router;
