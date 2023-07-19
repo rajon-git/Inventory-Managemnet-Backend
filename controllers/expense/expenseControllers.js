247fe90e67ee74c4aef02d566860cdd720586e62
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-19 16:44:39 +0600

    expense route added

--- controllers/expense/expenseControllers.js
+++ controllers/expense/expenseControllers.js
@@ -24,6 +24,20 @@ exports.CreateExpense = async (req, res) => {
 //   res.status(200).json(result);
 // };
 
+// Expense List
+exports.ExpenseList = async (req, res) => {
+    let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
+    let SearchArray = [
+      { Amount: SearchRgx },
+      { Note: SearchRgx },
+      { "Type.Name": SearchRgx },
+    ];
+    let JoinStage = {$lookup: {from: "expensetypes", localField: "TypeID", foreignField: "_id", as: "Type"}}
+    let result = await ListOneJoinService(req, ExpenseModel, SearchArray,JoinStage);
+    res.status(200).json(result);
+  };
+  
+
 // UpdateExpense
 exports.UpdateExpense = async (req, res) => {
     let result = await UpdateService(req, ExpenseModel);
