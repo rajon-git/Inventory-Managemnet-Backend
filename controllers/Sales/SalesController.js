cfbf4f3dc25d7b3d1e3bd4c3903f2d72869faa8e
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-24 17:58:56 +0600

    sales list controller added

--- controllers/Sales/SalesController.js
+++ controllers/Sales/SalesController.js
@@ -20,3 +20,32 @@ exports.CreateSales = async (req, res) => {
     );
     res.status(200).json(Result);
   };
+
+  // Sales List
+exports.SalesList = async (req, res) => {
+    let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
+    let SearchArray = [
+      { Note: SearchRgx },
+      { "customers.CustomerName": SearchRgx },
+      { "customers.Address": SearchRgx },
+      { "customers.Phone": SearchRgx },
+      { "customers.Email": SearchRgx },
+    ];
+    let JoinStage = {
+      $lookup: {
+        from: "customers",
+        localField: "CustomerID",
+        foreignField: "_id",
+        as: "customers",
+      },
+    };
+    let result = await ListOneJoinService(
+      req,
+      ParentModel,
+      SearchArray,
+      JoinStage
+    );
+    
+    res.status(200).json(result);
+  };
+  
