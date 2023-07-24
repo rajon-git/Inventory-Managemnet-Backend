a355cbfbd419ecee3b2d99155d8e60f4fb6eb32e
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-24 18:37:21 +0600

    delete service added

---
+++ services/common/deleteService.js
@@ -0,0 +1,28 @@
+/**
+ * Date: 24/07/2023
+ * Subject: Inventory Project purchase services
+ * Auth: Rajon
+ */
+
+const DeleteService = async (req, Model) => {
+    try {
+      let DeleteID = req.params.id;
+      let UserEmail = req.headers["email"];
+  
+      // Second Process
+      let QueryObj = {};
+      QueryObj["_id"] = DeleteID;
+      QueryObj[UserEmail] = UserEmail;
+  
+      // First DataBase Process
+      let Delete = await Model.deleteMany(QueryObj);
+  
+      // response
+      return {status: "success", delete: Delete};
+  
+    } catch (error) {
+      return { status: "fail", data: error.message };
+    }
+  };
+  
+  module.exports = DeleteService;
