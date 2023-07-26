f910a95d743ed849328e22085cc97c43e60bfa5a
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-26 14:25:28 +0600

    expense summary controller added

---
+++ controllers/Summary/summaryControllers.js
@@ -0,0 +1,17 @@
+/**
+ * Date: 26/07/2023
+ * Subject: Inventory Project Summary controller
+ * Auth: Rajon
+ */
+
+const ExpenseSummaryService = require("../../services/Summary/ExpenseSummaryService");
+const PurchaseSummaryService = require("../../services/Summary/PurchaseSummaryService");
+const ReturnSummaryService = require("../../services/Summary/ReturnSummaryService");
+const SalesSummaryService = require("../../services/Summary/SalesSummaryService");
+
+// ExpensesSummary
+exports.ExpensesSummary=async (req, res) => {
+    let Result=await ExpenseSummaryService(req)
+    res.status(200).json(Result);
+}
+
