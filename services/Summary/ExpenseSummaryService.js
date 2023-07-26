01003e8f1a7d83efd7ed8d4943adad374e87bf5f
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-26 14:19:37 +0600

    expense summary service added

---
+++ services/Summary/ExpenseSummaryService.js
@@ -0,0 +1,42 @@
+/**
+ * Date: 26/07/2023
+ * Subject: Inventory Project Summary Report Service
+ * Auth: Rajon
+**/
+const ExpenseModel = require("../../models/Expense/ExpensesModel");
+
+const ExpenseSummaryService = async (req) => {
+  try {
+    let UserEmail = req.headers["UserEmail"];
+
+    let data = await ExpenseModel.aggregate([
+      {
+        $match: {
+          UserEmail: UserEmail,
+        },
+      },
+      {
+        $facet: {
+          Total: [{ $group: { _id: 0, TotalAmount: { $sum: "$Amount" } } }],
+          Last30Days: [
+            {
+              $group: {
+                _id: {
+                  $dateToString: { format: "%Y-%m-%d", date: "$CreatedDate" },
+                },
+                TotalAmount: { $sum: "$Amount" },
+              },
+            },
+            { $sort: { _id: -1 } },
+            { $limit: 30 },
+          ],
+        },
+      },
+    ]);
+    return { status: "success", data: data };
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = ExpenseSummaryService;
